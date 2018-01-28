4f64f43cc5af63b0692bd08ec8bad1ee5cdf5575
Ajinkya Bagde <ajinkya.abhijit.bagde@gmail.com>
2018-01-27 23:01:55 -0800

    one read write cycle successful

--- Project2_rw_memory/byte_rotation.sv
+++ Project2_rw_memory/byte_rotation.sv
@@ -5,10 +5,12 @@ module byte_rotation(input logic clk, reset_n, start,
  output logic [31:0] mem_write_data,
  input logic [31:0] mem_read_data);
  
- enum logic [2:0] {IDLE=3'b000, READ_1=3'b001, READ_2=3'b010, WRITE_1=3'b011, WRITE_2=3'b100} state;
+ enum logic [2:0] {IDLE=3'b000, READ_1=3'b001, READ_2=3'b010, WRITE_1=3'b011, WRITE_2=3'b100, WRITE_3=3'b101} state;
  
  assign mem_clk = clk;
  logic [31:0] read_data;
+ //logic [31:0] write_data;
+ logic [31:0] num_words;
  
 function logic [31:0] byte_rotate(input logic [31:0] value);
  byte_rotate = {value[23:16], value[15:8], value[7:0], value[31:24]};
@@ -34,7 +36,7 @@ endfunction
 		 
 		READ_1: begin
 		   $display("IN READ_1");
-			mem_addr <= message_addr[31:16];
+			mem_addr <= message_addr[15:0];
 			$display(mem_addr);
 			mem_we <= 0;
 			state <= READ_2;
@@ -56,9 +58,10 @@ endfunction
 		WRITE_1: 
 		 if(mem_we) begin
 		      $display("IN WRITE_1, setting address");
+				num_words <= (size >> 4);
 				read_data <= mem_read_data;
 				$display(read_data);
-				mem_addr <= 4;
+				mem_addr <= output_addr[15:0];
 				state <= WRITE_2;
 		 end
 		
@@ -68,8 +71,16 @@ endfunction
 		      $display("IN WRITE_2, doing rotation");
 				//read_data <= mem_read_data;
 				$display("read_data: %x", read_data);
+				$display("num_words: %x", num_words);
 				mem_write_data <= byte_rotate(read_data);
-				mem_we <= 0;
+				state <= WRITE_3;
+		 end
+		 
+		  
+		WRITE_3:
+		 if(mem_we) begin
+		      mem_we <= 0;
+				$display("mem_write_data: %x", mem_write_data);
 				done <= 1;
 		 end
 		
